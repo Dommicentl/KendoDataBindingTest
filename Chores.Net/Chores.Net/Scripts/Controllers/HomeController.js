20887ce5a7713199e735cb9f8c70eebdcb6e197a
Leendert <dommicent.leendert@gmail.com>
2014-12-23 00:24:22 +0100

    Added angular and kendo + first form version with kendo

---
+++ Chores.Net/Chores.Net/Scripts/Controllers/HomeController.js
@@ -0,0 +1,10 @@
+﻿var app = angular.module('choresApp', []);
+
+app.controller('homeController', function($scope) {
+    $scope.surName = 'fill in your surName';
+    $scope.name = 'fill in your name';
+
+    $scope.changeValue = function() {
+        $scope.surName = 'changed';
+    };
+});
